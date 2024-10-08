3c2efe0096adedaf6473b3df498f1b8be1212d3f
Alexsandro Vicente <101852187+AlexsandroVcT@users.noreply.github.com>
2024-10-08 02:10:24 -0300

    login/register-front-finished

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,23 +1,37 @@
 import styles from '@/styles/Register.module.scss';
+import Link from 'next/link';
 import Image from 'next/image';
-import logo from '../../public/LogoZenLekOFCF.png';
 
 const Register = () => {
   return (
-    <div className={styles.registerContainer}>
-      <div className={styles.logoContainer}>
-        <Image src={logo} alt="ZenLek Finance" width={120} height={120} />
-        <h1>ZenLek Finance</h1>
-      </div>
-      <form className={styles.form}>
-        <input type="text" placeholder="Nome Completo" className={styles.input} />
-        <input type="email" placeholder="Email" className={styles.input} />
-        <input type="password" placeholder="Senha" className={styles.input} />
-        <input type="password" placeholder="Confirme a Senha" className={styles.input} />
-        <button type="submit" className={styles.button}>Cadastrar</button>
-      </form>
-      <div className={styles.login}>
-        <p>Já tem uma conta? <a href="/login">Faça login</a></p>
+    <div className={styles.registerPage}>
+      <div className={styles.registerBox}>
+        <Image src='/LogoZenLekOFCF.png' alt='ZenLek Finance' width={100} height={100} />
+        <h1>ZENLEK FINANCE</h1>
+        <p>Crie sua conta e tenha acesso aos melhores serviços financeiros.</p>
+        <form>
+          <div className={styles.inputGroup}>
+            <label htmlFor='cpf'>CPF</label>
+            <input type='text' id='cpf' name='cpf' placeholder='Digite seu CPF' />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' name='email' placeholder='Digite seu Email' />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor='senha'>Senha</label>
+            <input type='password' id='senha' name='senha' placeholder='Crie uma senha' />
+          </div>
+          <div className={styles.terms}>
+            Ao se cadastrar, você concorda com nossos
+            <Link href='#'> Termos de Serviço</Link> e a
+            <Link href='#'> Política de Privacidade</Link>.
+          </div>
+          <button type='submit' className={styles.registerButton}>Cadastrar</button>
+        </form>
+        <div className={styles.loginText}>
+          Já tem uma conta? <Link href='/login'>Faça login</Link>.
+        </div>
       </div>
     </div>
   );
